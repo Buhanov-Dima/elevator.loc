4c6065710d13ccdb18c1c8598a79114a2bbde0cd
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2019-07-28 16:00:17 +0300

    add graph
    ...

--- js/main.js
+++ js/main.js
@@ -54,6 +54,126 @@ $( document ).ready(function() {
 			$('.popup').removeClass('active');
 		}		
 	});
-	
-	
+
+	$('#monitor').click(function(){
+		$('.monitoring').removeClass('d-none');
+	});
+
+/*
+	$.getJSON("http://95.213.193.6:9777/api/predictions/1/102", function( data ) {
+	    console.log(data);
+
+	   	var ctx = document.getElementById('myChart');
+		var myChart = new Chart(ctx, {
+			type: 'line',
+			data: data,
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero: false
+		                }
+		            }]
+		        }
+		    }
+		});
+
+
+
+
+		var data = {
+			labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+			datasets: [{
+				label: "Dataset",
+				backgroundColor: "rgba(255,99,132,0.2)",
+				borderColor: "rgba(255,99,132,1)",
+				hoverBackgroundColor: "rgba(255,99,132,0.4)",
+				hoverBorderColor: "rgba(255,99,132,1)",
+				borderWidth: 2,
+				data: [65, 59, 20, 81, 56, 55, 40],
+			}]
+		};
+	});
+
+*/
+
+$.getJSON("http://95.213.193.6:9777/api/predictions/1/102", function (cdata) {
+
+		console.log(cdata);
+
+		var labeldata = [];
+
+		var chrtdata = [];
+
+		for(var i =0; i < cdata.length; i++)
+		{
+			labeldata[i] = cdata[i]["rul"];
+			chrtdata[i] = cdata[i]["timestamp"];
+//		  labeldata.push(cdata[i].["timecycle"]])
+		}
+		
+		console.log(chrtdata);
+		console.log(labeldata);
+
+		var ctx = document.getElementById("myChart").getContext("2d");
+
+		var myChart = new Chart(ctx, {
+		  type: 'line',
+		  data: {
+	        labels: chrtdata,
+	        datasets: [{
+	            label: '# Остаточный срок службы',
+	            data: labeldata,
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+
+		});
+
+		
+
+	});
+
 });
+
+/*		for(var item in result){
+              labels.push(result[item].slice(0,1).toString());
+              data.push(result[item].slice(1,2).toString());
+          }
+
+	    var tempData = {
+	        labels : labels,
+	        datasets : [{
+	            fillColor : "rgba(172,194,132,0.4)",
+	            strokeColor : "#ACC26D",
+	            pointColor : "#fff",
+	            pointStrokeColor : "#9DB86D",
+	            data : data
+	        }]
+	    };
+    	var temp = document.getElementById('compradores').getContext('2d');
+    	var lineChart = new Chart(temp).Line(tempData);
+*/
+
+
+/*	fetch("http://95.213.193.6:9777/predictions/1")
+  		.then(resp => resp.json())
+  		.then(data => console.log(data)
+*/	
+	
